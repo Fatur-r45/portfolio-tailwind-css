af2d9a1ffbcfe1e6b1271f3a45afdad6876ea73e
Fatur Rahman Gandi <faragi691@gmail.com>
2022-04-29 03:31:27 +0700

    ada update baru

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,13 +8,26 @@ humberger.addEventListener("click", function () {
   navMenu.classList.toggle("hidden");
 });
 
+// klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != humberger && e.target != navMenu) {
+    humberger.classList.remove("humberger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
 // navbar-fixed
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
