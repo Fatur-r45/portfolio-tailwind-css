58892fc431df73fee926fd5b43a58e5370f495ad
Fatur-r45 <faragi691@gmail.com>
2023-09-09 14:03:55 +0700

    add somethink

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["index.html", "script.js"],
   darkMode: "class",
   theme: {
     container: {
